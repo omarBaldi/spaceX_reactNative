4bc7da16e585840a1ea909c4e39d002d4d2ce7d9
Omar Baldi <omarbaldidk@gmail.com>
2021-11-07 18:14:08 +0100

    Installed carousel library and implemented functionality to render images inside of it and fixed export bug in library installed

--- src/screens/element/element.tsx
+++ src/screens/element/element.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 import {
   DragonsI,
   MainCategories,
@@ -10,6 +10,7 @@ import {
 import { CustomText } from '../../atoms/text';
 import { TextHierarchy } from '../../atoms/text/dto';
 import APICustomHook from '../../hooks/API-hook';
+import Swiper from 'react-native-swiper';
 
 const ElementScreen = ({ route }: { route: any }) => {
   const {
@@ -39,8 +40,8 @@ const ElementScreen = ({ route }: { route: any }) => {
           cost_per_launch,
           country,
           success_rate_pct,
+          flickr_images,
         } = current as RocketsI;
-        console.log(current);
         return (
           <View style={{ flex: 1, width: '100%', padding: 30 }}>
             <CustomText
@@ -50,44 +51,68 @@ const ElementScreen = ({ route }: { route: any }) => {
               value={rocketName}
             />
             {/* +--------------------------------------------------------- */}
+            <View style={{ height: 250, marginBottom: 30 }}>
+              <Swiper
+                autoplay={true}
+                autoplayTimeout={5}
+                dotColor='black'
+                activeDotColor='white'
+                dotStyle={{
+                  marginRight: 5,
+                  marginLeft: 5,
+                }}
+                activeDotStyle={{
+                  borderWidth: 1,
+                  borderColor: 'black',
+                  marginRight: 5,
+                  marginLeft: 5,
+                }}
+                paginationStyle={{
+                  bottom: 5,
+                }}
+              >
+                {flickr_images.map((currenImageSrc: string, index: number) => {
+                  return (
+                    <View
+                      key={index}
+                      style={{ overflow: 'hidden', borderRadius: 15 }}
+                    >
+                      <Image
+                        key={index}
+                        source={{ uri: currenImageSrc }}
+                        style={{ width: '100%', height: '100%' }}
+                      />
+                    </View>
+                  );
+                })}
+              </Swiper>
+            </View>
+            {/* +--------------------------------------------------------- */}
             <CustomText
               additionalStyle={{ marginBottom: 20 }}
               value={description}
               hierarchy={TextHierarchy.SECONDARY}
             />
             {/* +--------------------------------------------------------- */}
-            <CustomText
-              additionalStyle={{ marginBottom: 20 }}
-              value='Height'
-              hierarchy={TextHierarchy.SECONDARY}
-            />
+            <CustomText value='Height' hierarchy={TextHierarchy.SECONDARY} />
             <CustomText
               additionalStyle={{ marginBottom: 20 }}
               value={rocketHeight}
             />
             {/* +--------------------------------------------------------- */}
-            <CustomText
-              additionalStyle={{ marginBottom: 20 }}
-              value='Diameter'
-              hierarchy={TextHierarchy.SECONDARY}
-            />
+            <CustomText value='Diameter' hierarchy={TextHierarchy.SECONDARY} />
             <CustomText
               additionalStyle={{ marginBottom: 20 }}
               value={`${rocketDiameter} M`}
             />
             {/* +--------------------------------------------------------- */}
-            <CustomText
-              additionalStyle={{ marginBottom: 20 }}
-              value='Mass'
-              hierarchy={TextHierarchy.SECONDARY}
-            />
+            <CustomText value='Mass' hierarchy={TextHierarchy.SECONDARY} />
             <CustomText
               additionalStyle={{ marginBottom: 20 }}
               value={`${new Intl.NumberFormat('en-US').format(Number(kg))} KG`}
             />
             {/* +--------------------------------------------------------- */}
             <CustomText
-              additionalStyle={{ marginBottom: 20 }}
               value='Cost for launch'
               hierarchy={TextHierarchy.SECONDARY}
             />
@@ -97,7 +122,6 @@ const ElementScreen = ({ route }: { route: any }) => {
             />
             {/* +--------------------------------------------------------- */}
             <CustomText
-              additionalStyle={{ marginBottom: 20 }}
               value='Success rate'
               hierarchy={TextHierarchy.SECONDARY}
             />
@@ -151,7 +175,9 @@ const ElementScreen = ({ route }: { route: any }) => {
       {!elementData || !Object.keys(elementData).length ? (
         <></>
       ) : (
-        renderElementData(elementData as MainCategoryProps)
+        <ScrollView>
+          {renderElementData(elementData as MainCategoryProps)}
+        </ScrollView>
       )}
     </View>
   );
